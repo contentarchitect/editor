8f525b27a07303a47181f32609ab7417d6fef847
Cihad Paksoy <cihadp@gmail.com>
2019-11-30 18:20:30 +0300

    title is unnecessary block, because header block is doing same thing with it

--- src/blocks/index.js
+++ src/blocks/index.js
@@ -1,7 +1,6 @@
 import { Blocks } from '@contentarchitect/editor'
 
 import Wysiwyg from '@/blocks/wysiwyg/main.js'
-import Title from "@/blocks/title/main.js"
 import Image from "@/blocks/image/main.js"
 import Spacer from "@/blocks/spacer/main.js"
 import Divider from "@/blocks/divider/main.js"
@@ -11,7 +10,6 @@ import Code from '@/blocks/code/main.js'
 import Header from '@contentarchitect/header'
 
 Blocks.register(Wysiwyg)
-Blocks.register(Title)
 Blocks.register(Image)
 Blocks.register(Spacer)
 Blocks.register(Divider)
@@ -24,7 +22,6 @@ window.blocksIndex = Blocks;
 
 export {
     Wysiwyg,
-    Title,
     Image,
     Spacer,
     Divider,

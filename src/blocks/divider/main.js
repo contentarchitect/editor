d5767520439dea48f7399918cba0fae092017657
Cihad Paksoy <cihadp@gmail.com>
2020-01-26 18:24:51 +0300

    Divider block options removed

--- src/blocks/divider/main.js
+++ src/blocks/divider/main.js
@@ -1,50 +1,12 @@
 import { Block } from "@contentarchitect/core"
-import view from "./view.vue"
-import settings from "./settings.vue"
+import View from "./view.vue"
 
 export default class Divider extends Block {
 	static get viewComponent () {
-		return view;
+		return View;
 	}
-
-	static get settingsComponent () {
-		return settings;
-	}
-
-	static defaultData () {
-		return {
-			dividerType: 'star'
-		}
-	}
-
-	static defaultSettings = {
-		types: ['star', 'border']
-	}
-
 
 	toHTML () {
-		switch(this.dividerType) {
-			case "star":
-				return `<div class="divider divider-${this.dividerType}"></div>`
-			case "border":
-				return `<div class="divider divider-${this.dividerType}"><div></div></div>`
-		}
-	}
-
-	static serializeFromHTML (html) {
-		const classes = html.querySelector(".divider").classList;
-
-		let dividerType;
-		if (classes.contains("divider-" + "star")) {
-			dividerType = "star"
-		} else if (classes.contains("divider-" + "border")) {
-			dividerType = "border"
-		} else {
-			dividerType = "star"
-		}
-
-		return {
-			dividerType
-		}
+		return `<div class="divider"></div>`
 	}
 }
--- src/blocks/divider/view.vue
+++ src/blocks/divider/view.vue
@@ -1,26 +1,7 @@
 <template>
-  <component :is="value.dividerType" />
+	<div class="divider"></div>
 </template>
 
 <script>
-export default {
-    props: ['value'],
-    computed: {
-        typeClass () {
-            return "divider-" + this.dividerType;
-        }
-    },
-    components: {
-        star: {
-            render (createElement) {
-                return createElement("hr", { attrs: {  class: "divider divider-star" } })
-            }
-        },
-        border: {
-            render (h) {
-                return h("hr", { attrs: { class: "divider divider-border" } })
-            }
-        },
-    }
-}
+export default {}
 </script>

5883749d434e1f66ce1842d7f856f1de5a1970f6
Cihad Paksoy <cihadp@gmail.com>
2020-02-25 06:25:38 +0300

    fix caption text

--- src/blocks/table/main.js
+++ src/blocks/table/main.js
@@ -75,7 +75,7 @@ export default class Table extends Block {
         if (!table) return this.defaultData()
 
         let caption = table ? table.caption : ""
-        caption = caption || ""
+        caption = caption.innerHTML || ""
 
         const data = []
 
